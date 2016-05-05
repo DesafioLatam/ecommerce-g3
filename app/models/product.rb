64f4a96dbcf650e163d386ce408d2befbcedb73a
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2016-05-04 22:41:08 -0300

    validating the presence of the price in products, orders of product are working

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :orders
   has_many :users, through: :orders
+  validates :price, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Order.destroy_all
 Product.destroy_all
 Category.destroy_all
 
 c = Category.create(name: "Teléfono")
-Product.create(name: "Galaxy 6", photo: "", stock: 10, category: c, created_at: Date.today - 2.day)
-Product.create(name: "Galaxy 5", photo: "", stock: 5, category: c, created_at: Date.today - 1.day)
-Product.create(name: "Iphone 6", photo: "", stock: 10, category: c)
+Product.create(name: "Galaxy 6", photo: "", stock: 10, category: c, price:100, created_at: Date.today - 2.day)
+Product.create(name: "Galaxy 5", photo: "", stock: 5, category: c, price:200, created_at: Date.today - 1.day)
+Product.create(name: "Iphone 6", photo: "", stock: 10, category: c, price:300)
 
 c = Category.create(name: "Notebooks")
-Product.create(name: "Macbook Pro", photo: "", stock: 20, category: c)
+Product.create(name: "Macbook Pro", photo: "", stock: 20, category: c, price:400)
 
 u = User.create(email: "gonzalo@desafiolatam.com", password:"12345678")
