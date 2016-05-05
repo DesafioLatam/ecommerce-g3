f7ae0e37182601c1d8199d630ab4a61e3b2a9def
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2016-05-04 22:28:45 -0300

    improvements on UI in layout and products#index view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,9 @@ class OrdersController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-    @order = @product.orders.build(user:current_user)
-    if @order.save
+    current_user.last_cart.orders << Order.new(user:current_user)
+
+    if current_user.last_cart.save
       redirect_to products_path, notice: "La orden ha sido guardada"
     else
       redirect_to products_path, alert: "La orden no ha podido ser guardada"
